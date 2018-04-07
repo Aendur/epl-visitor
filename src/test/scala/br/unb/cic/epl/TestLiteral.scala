26d65ea429bb4d5932ebf2e8f96cbe78ec82677d
aendur <aendur@users.noreply.github.com>
2018-04-07 02:12:47 -0300

    + Literal test

---
+++ src/test/scala/br/unb/cic/epl/TestLiteral.scala
@@ -0,0 +1,28 @@
+package br.unb.cic.epl
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import org.scalatest.GivenWhenThen
+import org.scalatest.BeforeAndAfter
+
+
+class TestLiteral extends FlatSpec with Matchers with GivenWhenThen with BeforeAndAfter {
+
+  behavior of "A Literal"
+
+  var literalX: Literal = _
+
+  before {
+    literalX = new Literal(-5) 
+  }
+
+  it should "return String -5 when we call Literal(-5).print()" in {
+    literalX.print() should be ("-5")
+  }
+
+  it should "return -5 when we call Literal(-5).eval()" in {
+    val eval = new Eval()
+    literalX.accept(eval)
+    eval.result() should be (-5)
+  }
+}
